9cf076f40823456b937d03d35aa052d68fe69a80
Faisal7007 <126740052+Faisal7007@users.noreply.github.com>
2023-07-25 14:23:45 +0530

    commit

--- src/newsItem/NewsItem.jsx
+++ src/newsItem/NewsItem.jsx
@@ -5,14 +5,15 @@ function NewsItem({image,title,publishedAt,url}) {
   return (
     
     <div className='news_item'>
-      <a href={url} target='_blank'>
         <img className='news_img' src={image?image:'https://cdn3.vectorstock.com/i/1000x1000/37/07/not-available-sign-or-stamp-vector-22523707.jpg'} alt="image" />
-    </a>
+        <a className='links' href={url} target='_blank'>
 
         <div className="details">
         <h1 className='title'>{title.slice(0,95)+'...'}</h1>
         <h3 className='time' >{new Date(publishedAt).toGMTString()}</h3>
         </div>
+
+    </a>
     </div>
 
   )
